d7377f38a79160d0ad89a1d31c5517d19b5d20fb
Alex Manzi <alex.manzi22@gmail.com>
2022-03-28 15:22:00 -0400

    login page +sync up

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../Login.css';
 
 function Login() {
   return (
     <div>
-        <h1>login</h1>
+        <div id="LoginBox">
+          <h1>Logo Here</h1>
+            <div id="Loginheader">
+                <h3>Login</h3>
+                <h4>Enter your info</h4>
+            </div>
+                <input id="Userinputs" type="text" placeholder="Enter Your Email"/>
+                <input id="Userinputs" type="text" placeholder="Enter Your Password"/>
+            <button id="SubmitButton" type="button">Submit</button>
+        </div>
+        
+    
     </div>
   )
 }
