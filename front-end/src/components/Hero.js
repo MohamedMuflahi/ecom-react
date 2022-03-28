8ed6e309d34ccdf01a087cd2b3c84556a79bcc7a
mohamed muflahi <muflahimohamed88@gmail.com>
2022-03-28 15:21:28 -0400

    homepage structure

---
+++ front-end/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Hero.css';
+function Hero() {
+  return (
+    <div className='container'>
+        <img src="https://i.ibb.co/SryjdK7/Background.png" alt="hero" className='img'/>
+    </div>
+  )
+}
+
+export default Hero
