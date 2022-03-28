3af319f6832ca4af6b715c9bf7ad5be0551bcc2f
Alex Manzi <alex.manzi22@gmail.com>
2022-03-28 17:29:33 -0400

    cart start

---
+++ front-end/src/components/Cartitem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Cartitem.css';
+
+function Cartitem() {
+  return (
+    <div id="cardDisplay">
+        <img src="https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png" alt="placeholder"/>
+        <div id="ItemInfo">
+            <h2 id="ItemInfo">Price</h2>
+            <h2 id="ItemInfo">Qty: </h2>
+            <h2 id="ItemInfo">Product Name</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Cartitem
--- front-end/src/pages/cart.js
+++ front-end/src/pages/cart.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Cartitem from '../components/Cartitem'
 
 function Cart() {
   return (
-    <div>cart</div>
+    <div>
+      <Navbar />
+      <div id="CartText">
+        <h1>Here's what you have in your cart.</h1>
+        <h2>100% Free deliver and returns.</h2>
+        <Cartitem />
+      </div>
+      
+    </div>
   )
 }
 
