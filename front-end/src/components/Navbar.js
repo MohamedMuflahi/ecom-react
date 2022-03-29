858c47a96cfebd31585ba2efe381d99075ff684f
mohamed muflahi <muflahimohamed88@gmail.com>
2022-03-29 10:41:32 -0400

    added structure to Home

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -18,6 +18,9 @@ function Navbar() {
         <a className='item'>
             <h1 className='item-name'>Cart</h1>
         </a>
+        <a className='ham'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/250px-Hamburger_icon.svg.png"/>
+        </a>
       </div>
   )
 }
--- front-end/src/pages/home.js
+++ front-end/src/pages/home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Products from '../components/Products';
+
 function Home() {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const chevronWidth = 40;
   return (
     <div>
       <Navbar/>
       <Hero />
+      <Products />
     </div>
   )
 }
