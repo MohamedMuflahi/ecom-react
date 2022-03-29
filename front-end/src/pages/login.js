6c845087adb9025941a5b2a568d583f76ebe333c
mohamed muflahi <muflahimohamed88@gmail.com>
2022-03-29 13:45:55 -0400

    login responsive fix

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -5,19 +5,14 @@ function Login() {
   return (
     <div id="background">
         <div id="LoginBox">
-          <h1 id="Logoline">Logo Here</h1>
+            <img id='logo' src='https://i.ibb.co/KrDfhMc/2-0-500-250-px-250-250-px.png'/>
             <div id="Loginheader">
-                <h3>Login</h3>
-                <h4>Enter your info here</h4>
+                <h3>Log in</h3>
+                <p>Continue to Shopify 2.0</p>
             </div>
                 <input id="Userinputs" type="text" placeholder="Enter Your Email"/>
                 <input id="Userinputs" type="text" placeholder="Enter Your Password"/>
             <button id="SubmitButton" type="button">Submit</button>
-            <div id="Bottomtext">
-              <span>Help   </span>
-              <span>Privacy   </span>
-              <span>Terms</span>
-            </div>
         </div>
         
     
