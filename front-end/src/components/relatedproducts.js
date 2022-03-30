ac9e599f659b7f6fa74e264b7d1242ea89e5eac9
Alex Manzi <alex.manzi22@gmail.com>
2022-03-30 12:44:44 -0400

    Wednesday Lunch

---
+++ front-end/src/components/relatedproducts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Relatedproducts.css';
+
+function relatedProducts() {
+  return (
+    <div id="cardDisplay">
+        <img id="relatedpic" src="https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png" alt="placeholder"/>
+        <div id="ItemInfo">
+            <h2 id="ItemInfo">Product Name</h2>
+            <h3 id="ItemInfo">Price</h3>
+        </div>
+    </div>
+  )
+}
+
+export default relatedProducts
