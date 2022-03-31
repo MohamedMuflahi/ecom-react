c7b0f7995f03e836f3b024b566ef0fa198b804f0
Alex Manzi <alex.manzi22@gmail.com>
2022-03-31 09:29:09 -0400

    Thurs changes

--- front-end/src/pages/cart.js
+++ front-end/src/pages/cart.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Navbar from '../components/Navbar'
 import Cartitem from '../components/Cartitem'
 import { Link } from 'react-router-dom'
 import "./cart.css"
 
 function Cart() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem('cart')))
 
+  }, [])
+  let total = 0;
+  cart.forEach(e=> total += e.price)
+  let tax = (Math.floor((total*0.0875) * 100) / 100).toFixed(2)
   return (
     <div>
       <Navbar />
@@ -14,12 +21,47 @@ function Cart() {
         <h2>100% Free deliver and returns.</h2>
         <Link to="/"><button id="ContinueShopping" type="button">CONTINUE SHOPPING</button></Link>
         </div>
-
-      <div id="CartText">
+        <div className='cart-container'>
+          <div className=''></div>
+          <div className='cart-box cart-items'>
+          {cart.map((item)=>{
+        return (
+          <>
+          <Cartitem key={item.id} item={item} setCart={setCart}/>
+          <hr></hr>
+          </>
+        )
+      })}
+          </div>
+          <div className='cart-box cart-checkout'>
+              <h1>Order Summary</h1>
+          <div className='total-container'>
+            <div>
+              <p>Subtotal:</p>
+              <p>Estimated Shipping & Handlinng:</p>
+              <p>Estimated Tax:</p>
+            </div>
+            <div className='total-item'>
+              <p>${total}</p>
+              <p>$0</p>
+              <p>${tax}</p>
+            </div>
+          </div>
+            <hr></hr>
+            <div className='total-container'>
+            <p>Total</p>
+            <p className='total-item'>${(Math.floor((total + (total*0.0875)) * 100) / 100).toFixed(2)}</p>
+            </div>
+            <hr></hr>
+            <button>Checkout</button>
+          </div>
+          <div className=''></div>
+        </div>
+      {/* <div id="CartText">
         <div id="ItemDiv">
-        <Cartitem />
-        <Cartitem />
-        <Cartitem />
+        {cart.map((item)=>{
+        return <Cartitem key={item.id} item={item}/>
+      })}
         </div>
         <div id="OrderSummary">
           <h1>ORDER SUMMARY</h1>
@@ -28,12 +70,12 @@ function Cart() {
           <h2>Estimated Total</h2>
           <button id="CheckoutButton" type="button" >CHECKOUT NOW</button>
           <div id="paymentHere">
-            <h4>Payment methods</h4>
+            <button><h4>Payment methods</h4></button>
             </div>
           <img id="payment" src="https://help.zazzle.com/hc/article_attachments/360010513393/Logos-01.png" alt="paymentoptions"/>
           
         </div>
-      </div>
+      </div> */}
       
     </div>
   )
