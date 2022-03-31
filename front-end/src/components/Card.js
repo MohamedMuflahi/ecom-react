d8efec82a15b586ba236a40b454d7dec6c1c3b9a
Alex Manzi <alex.manzi22@gmail.com>
2022-03-31 09:50:22 -0400

    thursday changes

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./Card.css";
 function Card({item}) {
-  return (
+    function handleClick(){
+      let cart = JSON.parse(localStorage.getItem('cart'));
+      let newcart = [...cart,item]
+      localStorage.setItem('cart',JSON.stringify(newcart))
+    }
+
+    return (
     <li className='card'>
       <img src={item.image} alt="cardImage" />
       <h4>{item.title}</h4>
       <p>{`Price $ ${item.price}`}</p>
-        <button className="primary">
+        <button className="primary" onClick={handleClick}>
           Add to Cart
         </button>
     </li>
