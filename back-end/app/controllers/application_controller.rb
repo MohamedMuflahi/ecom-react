fe47c5c7f7e51ca206719568b4d0f15affffb737
mohamed muflahi <muflahimohamed88@gmail.com>
2022-03-31 20:24:16 -0400

    stripe api implementation

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,11 +1,35 @@
 class ApplicationController < Sinatra::Base
   require 'bcrypt'
+  require 'stripe'
   set :default_content_type, 'application/json'
   
   configure do
     enable :sessions 
     set :session_secret, "secret"
   end
+  
+    require 'stripe'
+    
+    # This is your test secret API key.
+    Stripe.api_key = 'sk_test_51KhJDiCpxluX9yaNmQgu9KokMoY17k9SzMuZp9xZntpIjjnYo1ejKeEgcDryxtP48W4i0WQXUsrW1TeBU55jmsDP00xTgI1KTF'
+
+    YOUR_DOMAIN = 'http://localhost:3000'
+    
+    post '/create-checkout-session' do
+        content_type 'application/json'
+
+        session = Stripe::Checkout::Session.create({
+            line_items: [{
+      # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+      price: "price_1KjTg5CpxluX9yaNrWetref5",
+      quantity: 1,
+    }],
+    mode: 'payment',
+    success_url: YOUR_DOMAIN + '/success',
+    cancel_url: YOUR_DOMAIN + '/canceled',
+    })
+    return {url: session.url}.to_json
+end
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
